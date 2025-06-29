a0aa306cfb7cd3c091264ca2e5691fd47f24e559
skrish2020 <skrish@mantrainc.com>
2025-06-28 18:47:37 -0700

    added vars

--- atlantis/templates/start-atlantis.sh
+++ atlantis/templates/start-atlantis.sh
@@ -7,4 +7,5 @@ set -e
 --gh-user="$USERNAME" \
 --gh-token="$TOKEN" \
 --gh-webhook-secret="$SECRET" \
---repo-allowlist="$ALLOW_LIST"
+--repo-allowlist="$ALLOW_LIST" \
+--repo-config="$REPO_CONFIG" 
